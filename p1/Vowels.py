01b44b729c397d567df6672b37c5275caf6dc3c1
sowmya595 <rsowmya1997@msitprogram.net>
2019-01-28 17:47:38 +0530

    vowels

---
+++ p1/Vowels.py
@@ -0,0 +1,8 @@
+s=str(input("enter string"))
+count=0;
+for vowel in s:
+    if vowel=='a' or vowel=='e' or vowel=='i' or vowel=='o' or vowel=='u':
+        count=count+1
+
+print("number of vowels"+str(count))
+    
